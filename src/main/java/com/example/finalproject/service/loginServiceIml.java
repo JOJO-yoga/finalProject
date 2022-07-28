75fc0ac35f0a1010164cc93ece31cdace67d2f75
Yoga <1459346364@qq.com>
2022-07-28 21:28:28 +0100

    initial

--- src/main/java/com/example/finalproject/service/loginServiceIml.java
+++ src/main/java/com/example/finalproject/service/loginServiceIml.java
@@ -13,34 +13,23 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-/**
- * @author 张子行
- * @class UserDetailsService
- */
+
 @Service("userDetailsService")
 class MyUserDetailsService implements UserDetailsService {
 
     @Autowired
     private userDao userDao;
 
-    /**
-     * @param
-     * @method 登录逻辑
-     */
     @Override
     public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
-        //调用usersMapper方法，根据用户名查询数据库
         user user = userDao.findUserByName(name);
 
-        //判断
-        if (user == null) {//数据库没有用户名，认证失败
-            throw new UsernameNotFoundException("用户名不存在！");
+        if (user == null) {
+            throw new UsernameNotFoundException("User not exist！");
         }
 
-        //角色要加前缀ROLE_ ，权限直接没啥要求
         List<GrantedAuthority> auths =
                 AuthorityUtils.commaSeparatedStringToAuthorityList("admins,ROLE_sale");
-        //从查询数据库返回users对象，得到用户名和密码，返回
         return new org.springframework.security.core.userdetails.User(user.getName(),
                 new BCryptPasswordEncoder().encode(user.getPassword()), auths);
     }
